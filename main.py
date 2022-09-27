aad35c3ed41cf413ffd9398ea58d7ab9a20a7e18
Rui Pedro <ruipedro.costa.594@gmail.com>
2022-09-27 14:16:29 +0100

    new menus in docs, main building

--- main.py
+++ main.py
@@ -1,6 +1,123 @@
 from marketplace import Marketplace
+import docs
 
-if if __name__ == '__main__':
+if __name__ == '__main__':
 
     command = ''
     marketplace = Marketplace.create_marketplace()
+    user_id = 0
+    print(docs.LOGO1)
+    print(docs.MENU)
+
+    while command is not None:
+
+        command = input("Please select an option: ")
+
+        match command:
+
+            case 's':
+
+                cmd = ''
+
+                while cmd is not None:
+
+                    print(docs.SELECT)
+                    cmd = input("Please select an option: ")
+
+                    match cmd:
+
+                        case 'nb':
+
+
+
+                        case 'cb':
+
+
+
+                        case 'h':
+
+                            print(docs.SELECT)
+
+                        case 'm':
+
+                            cmd = None
+
+                        case 'e':
+
+                            cmd = None
+                            command = None
+
+
+            case 'b':
+
+                print(docs.BUYER2)
+                cmd = ''
+
+                while cmd is not None:
+
+                    cmd = input("Please select an option: ")
+
+                    match cmd:
+
+                        case 'clc':
+
+                        case 'cst':
+
+                        case 'bc':
+
+                        case 'cc':
+
+                        case 'ac':
+
+                        case 'cs':
+
+                        case 'h':
+
+                            print(docs.BUYER2)
+
+                        case 'm':
+
+                            cmd = None
+
+                        case 'e':
+
+                            cmd = None
+                            command = None
+
+            case 'a':
+
+                print(docs.ADMIN)
+                cmd = ''
+
+                while cmd is not None:
+
+                    cmd = input("Please select an option: ")
+
+                    match cmd:
+
+                        case 'lcd':
+
+                        case 'ucd':
+
+                        case 'lp':
+
+                        case 'lps':
+
+                        case 'cmp':
+
+                        case 'h':
+
+                            print(docs.ADMIN)
+
+                        case 'm':
+
+                            cmd = None
+
+                        case 'e':
+
+                            cmd = None
+                            command = None
+
+            case 'e':
+
+                command = None
